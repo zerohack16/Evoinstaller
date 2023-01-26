987ee17c6a308e89f1a15816b43cb658f93b21c1
Juan Rangel Díaz <zerohack16@hotmail.com>
2023-01-26 17:59:11 -0600

    Update evoinstaller.sh
    beta del script de instalacion de tema evolution

--- evoinstaller.sh
+++ evoinstaller.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 # Descripción: mini script para la instalacion de tema evolution e actualización de directadmin
 
 echo "Updating directadmin"
@@ -19,3 +19,22 @@ echo "Updating DA version"
 /usr/local/directadmin/custombuild/build version
 echo "Finishing update"
 /usr/local/directadmin/custombuild/build update
+
+echo "Creando directorio para el skin evolution"
+mkdir /usr/local/directadmin/data/skins/evolution
+
+echo "descargando el archivo necesario"
+wget -O /usr/local/directadmin/data/skins/evolution http://demo.directadmin.com/download/evolution.tar.gz
+
+echo "descrompimiendo el archivo"
+tar -xvzf evolution.tar.gz -C /usr/local/directadmin/data/skins/evolution
+
+echo "realizando ajustes en el skin"
+mv /usr/local/directadmin/data/skins/enhanced /usr/local/directadmin/data/skins/enhancedORG
+cp /usr/local/directadmin/data/skins/evolution /usr/local/directadmin/data/skins/enhanced
+
+echo "reiniciando diredadrmin"
+service directadmin restart
+
+echo "proceso concluido"
+echo "++++++++++++++++++++++++++++"
